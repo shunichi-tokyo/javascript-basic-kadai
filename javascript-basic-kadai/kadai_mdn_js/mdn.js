8afe74c5774a1f3fc012d701f83261ce2a241e30
shunichi-tokyo <pcshunichi.higa@gmail.com>
2025-03-26 11:11:38 +0900

    作成

---
+++ javascript-basic-kadai/kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  return `${year}年${month}月${day}日`;
+}
+
+const now = new Date();
+console.log(formatDate(now));
